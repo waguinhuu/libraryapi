58650e3f249ea6f0d5eb819315a56c491f42d15d
WagnerSilva <wagnersillva909@gmail.com>
2025-05-04 17:11:43 -0300

    Enabling AuthorizationServerSettings

--- libraryapi/src/main/java/io/github/waguinhuu/libraryapi/config/AuthorizationServerConfiguration.java
+++ libraryapi/src/main/java/io/github/waguinhuu/libraryapi/config/AuthorizationServerConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
@@ -56,7 +57,9 @@ public class AuthorizationServerConfiguration {
     public TokenSettings tokenSettings() {
         return TokenSettings.builder()
                 .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                // access_token: token utilizado nas requisições
                 .accessTokenTimeToLive(Duration.ofMinutes(60))
+                // refresh_token: token para renovar o access_token
                 .refreshTokenTimeToLive(Duration.ofMinutes(90))
                 .build();
     }
@@ -94,6 +97,26 @@ public class AuthorizationServerConfiguration {
     @Bean
     public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource){
         return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings(){
+        return AuthorizationServerSettings.builder()
+                // obter token
+                .tokenEndpoint("/oauth2/token")
+                // para consultar status token
+                .tokenIntrospectionEndpoint("/oauth2/introspect")
+                // revogar token
+                .tokenRevocationEndpoint("/oauth2/revoke")
+                // authorization endpoint
+                .authorizationEndpoint("/oauth2/authorize")
+                // informaçoes do usuario OPEN ID CONNECT
+                .oidcUserInfoEndpoint("/oauth2/userinfo")
+                // obter a chave publica pra verificar a assinatura do token
+                .jwkSetEndpoint("/oauth2/jwks")
+                // logout
+                .oidcLogoutEndpoint("/oauth2/logout")
+                .build();
     }
 
 }
